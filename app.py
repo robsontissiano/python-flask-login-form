04e16c3425ec693790c4926f806eddc503cdda66
Robson Goncalves <robson@mapyourproperty.com>
2022-09-04 00:24:21 -0700

    Formated lint using black and split User model into models file

--- app.py
+++ app.py
@@ -10,88 +10,95 @@ from validators import user_validator, password_validator
 from models.user import db, User
 
 app = Flask(__name__)
-# later on
 db.init_app(app)
 
-
-# app = Flask(__name__)
-# db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'thisisasecretkey'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = "thisisasecretkey"
 
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = "login"
+
 
 @login_manager.user_loader
 def load_user(user_id):
-    """ Reload the user object from the user id stored in the session """
+    """Reload the user object from the user id stored in the session"""
     return User.query.get(int(user_id))
 
 
 class RegisterForm(FlaskForm):
-    """ Form for Register User """
-    username = StringField(validators=user_validator, render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=password_validator, render_kw={"placeholder": "Password"})
+    """Form for Register User"""
 
-    submit = SubmitField('Register')
+    username = StringField(
+        validators=user_validator, render_kw={"placeholder": "Username"}
+    )
+    password = PasswordField(
+        validators=password_validator, render_kw={"placeholder": "Password"}
+    )
+
+    submit = SubmitField("Register")
 
     def validate_username(self, username):
-        """ Method to check if user exists """
-        existing_user_username = User.query.filter_by(
-            username=username.data).first()
+        """Method to check if user exists"""
+        existing_user_username = User.query.filter_by(username=username.data).first()
         if existing_user_username:
             raise ValidationError(
-                'That username already exists. Please choose a different one.')
+                "That username already exists. Please choose a different one."
+            )
 
 
 class LoginForm(FlaskForm):
-    """ Form for Log a User in """
-    username = StringField(validators=user_validator, render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=password_validator, render_kw={"placeholder": "Password"})
+    """Form for Log a User in"""
 
-    submit = SubmitField('Login')
+    username = StringField(
+        validators=user_validator, render_kw={"placeholder": "Username"}
+    )
+    password = PasswordField(
+        validators=password_validator, render_kw={"placeholder": "Password"}
+    )
+
+    submit = SubmitField("Login")
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    """ Main endpoint to home index """
-    return render_template('home.html')
+    """Main endpoint to home index"""
+    return render_template("home.html")
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    """ Method that logs a user in and handle the login form """
+    """Method that logs a user in and handle the login form"""
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
-                return redirect(url_for('dashboard'))
-    return render_template('login.html', form=form)
+                return redirect(url_for("dashboard"))
+    return render_template("login.html", form=form)
 
 
-@app.route('/dashboard', methods=['GET', 'POST'])
+@app.route("/dashboard", methods=["GET", "POST"])
 @login_required
 def dashboard():
-    """ Dashboard endpoint to access after login """
-    return render_template('dashboard.html')
+    """Dashboard endpoint to access after login"""
+    return render_template("dashboard.html")
 
 
-@app.route('/logout', methods=['GET', 'POST'])
+@app.route("/logout", methods=["GET", "POST"])
 @login_required
 def logout():
-    """ Method to log a user out """
+    """Method to log a user out"""
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for("login"))
 
 
-@ app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    """ Method that registers a user and handle the register form """
+    """Method that registers a user and handle the register form"""
     form = RegisterForm()
 
     if form.validate_on_submit():
@@ -99,9 +106,9 @@ def register():
         new_user = User(username=form.username.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
 
-    return render_template('register.html', form=form)
+    return render_template("register.html", form=form)
 
 
 if __name__ == "__main__":
--- models/user.py
+++ models/user.py
@@ -1,14 +1,12 @@
-
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model, UserMixin):
-    """ Model for User """
+    """Model for User"""
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-
-
-# https://stackoverflow.com/questions/9692962/flask-sqlalchemy-import-context-issue/9695045#9695045
