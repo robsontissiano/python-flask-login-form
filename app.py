87a7b160efd9d7d2007e76acb5f796bc1a750243
Robson Goncalves <robson@mapyourproperty.com>
2022-09-05 01:50:01 -0700

    Added error handler to login and register APIs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user, LoginManager, login_required, logout_user
 from flask_bcrypt import Bcrypt
@@ -35,14 +35,39 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Method that logs a user in and handle the login form"""
+    messages = []
+    message = ''
+    status = 200
+
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
+                # in case of successful login, render the dashboard
+                message = f'User {form.username.data} logged successfully.'
+                app.logger.info(message)
+                # check how to send message as an argument to redirect
                 return redirect(url_for("dashboard"))
-    return render_template("login.html", form=form)
+            else:
+                message = 'Wrong Password'
+                app.logger.error(message)
+                messages.append(message)
+                status = 400
+        else:
+            message = f'User {form.username.data} not found'
+            app.logger.error(message)
+            messages.append(message)
+            status = 400
+    # else:
+    #     messages.append('Form invalid')
+    #     status = 400
+
+    # In case of form not valid or user not found, or password incorrect,
+    # render login template and gets a 400 bad request error
+    # return render_template("login.html", form=form, message=messages), 400
+    return render_template("login.html", form=form, message=messages), status
 
 
 @app.route("/dashboard", methods=["GET", "POST"])
@@ -64,15 +89,45 @@ def logout():
 def register():
     """Method that registers a user and handle the register form"""
     form = RegisterForm()
+    messages = []
+    message = ''
+    status = 200
 
-    if form.validate_on_submit():
+    user = user = User.query.filter_by(username=form.username.data).first()
+
+    if user:
+        status = 400
+        message = f'User {form.username.data} already exists.'
+        # messages.append(message)
+        # return render_template("register.html", form=form, message=messages), status
+
+    if not user and form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for("login"))
 
-    return render_template("register.html", form=form)
+        try:
+            new_user = User(username=form.username.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+
+            # in case of successful register, redirect to login
+            message = f'User {form.username.data} registered successfully.'
+            app.logger.info(message)
+            messages.append(message)
+            # return redirect(url_for("login", message = messages)), status
+            # check how to send message as an argument to redirect
+            # return redirect(url_for("login"))
+            return render_template("login.html", form=form, message=messages), status
+        except NameError:
+            status = 400
+            message = f'It was not possible to add the user {form.username.data}.'
+    # else:
+    #     messages.append('Form invalid')
+    #     status = 400
+
+    messages.append(message)
+    app.logger.error(message)
+
+    return render_template("register.html", form=form, message=messages), status
 
 
 if __name__ == "__main__":
