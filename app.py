50fc94e3f5fd0d51c973cf2df1ad852eaab88b9f
Robson Goncalves <robson@mapyourproperty.com>
2022-09-05 02:28:11 -0700

    modified:   app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user, LoginManager, login_required, logout_user
 from flask_bcrypt import Bcrypt
 
